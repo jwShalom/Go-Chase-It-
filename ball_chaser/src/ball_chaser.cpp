2856540e12fa7dfcd5c703e330184c73f9e31b37
Samuel, JungWoong, Seo <124924697+jwShalom@users.noreply.github.com>
2023-07-22 23:24:21 +0900

    Update ball_chaser.cpp

--- ball_chaser/src/ball_chaser.cpp
+++ ball_chaser/src/ball_chaser.cpp
@@ -7,7 +7,6 @@ ros::ServiceClient client;
 
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z) {
-    // TODO: Request a service and pass the velocities to it to drive the robot
     ball_chaser::DriveToTarget srv;
     srv.request.linear_x = lin_x;
     srv.request.angular_z = ang_z;
@@ -27,7 +26,6 @@ void process_image_callback(const sensor_msgs::Image img)
     
     bool white_ball_presence = false;
 
-    // TODO: Loop through each pixel in the image and check if there's a bright white one
     // Then, identify if this pixel falls in the left, mid, or right side of the image
     // Depending on the white ball position, call the drive_bot function and pass velocities to it
     // Request a stop when there's no white ball seen by the camera
